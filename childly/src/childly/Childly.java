064846193c0123b7f6fd9bac6b7f7e17ae26deef
hv6kyh <hv6kyh@naver.com>
2019-08-10 15:39:50 +0900

    Add comments

--- childly/src/childly/Childly.java
+++ childly/src/childly/Childly.java
@@ -9,16 +9,12 @@ public class Childly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
 		Scanner sc = new Scanner(System.in);	// 표준 입력을 위해 스캐너 객체 이용
 		Stack<Integer> stack = new Stack<>();	// 스택 선언
 
 		int top = 1;							// 스택에 넣을 숫자
 		int n = -1;								// 사용자에게 입력 받는 숫자
-		
-		String masks[];
 
-		
 		while (n != 0) {
 			
 			do {
@@ -34,74 +30,45 @@ public class Childly {
 	
 			Node root = new Node("A");				// 최상위 노드는 A(push)부터 시작
 			
-			Tree tree = new Tree();
-			tree.addChild(root, n, 0);				// A와 B의 개수에 따라 자식 노드들을 만든다
+			Tree tree = new Tree();					// 트리 생성
+			tree.addChild(root, n, 0);				// 자식 노드들을 만든다
 	
+			// 최하위 레벨의 노드(단말 노드)들을 담은 리스트
 			ArrayList<Node> list = tree.getLastList();
 			
-			// 마스크를 보관하는 배열이 있다.
-			// 배열의 크기는 최종 노드의 개수와 같다
-			masks = new String[list.size()];
-			
-			// 배열에 마스크를 채운다
-			for (int i = 0; i < list.size() ; i++) {
-				masks[i] = tree.chase(list.get(i));
-			}
-			
-			tree.inOrder(root);
-			System.out.println("clear");
-			tree.clearTree(root);
-			root = null;
-			tree.inOrder(root);
-			
-			for (int i = masks.length-1; i >= 0; i--) {
+			// 리스트에 담긴 노드의 개수만큼 반복
+			for (int i = list.size() - 1; i >= 0 ; i--) {
 				
-				String mask = masks[i];
+				// 리스트에 담긴 노드를 하나씩 꺼낸다
+				Node node = list.get(i);
 				
+				// 그 노드의 부모들을 추적해 마스크를 만든다
+				String mask = tree.chase(node);
+				
+				// 마스크 문자열의 길이만큼 반복
 				for (int j = 0; j < mask.length(); j++) {
 					
 					switch (mask.charAt(j)) {
 					
+					// 문자가 A면 push
 					case 'A' :
 						stack.push(top++);
 						break;
 						
+					// 문자가 B면 pop
 					case 'B' :
 						System.out.print(stack.pop() + " ");
 						break;
 						
 					}
+					
 				}
+				
 				top = 1;
 				System.out.println();
 				
 			}
-	
-//			for (int i = list.size() - 1; i >= 0 ; i--) {
-//				
-//				// 리스트에 담긴 노드(트리 구조상 최하위 레벨의 노드들)
-//				Node node = list.get(i);
-//				// 그 노드의 부모들을 추적해 마스크를 만든다
-//				String mask = tree.chase(node);
-//				
-//				for (int j = 0; j < mask.length(); j++) {
-//					
-//					switch (mask.charAt(j)) {
-//					
-//					case 'A' :
-//						stack.push(top++);
-//						break;
-//						
-//					case 'B' :
-//						System.out.print(stack.pop() + " ");
-//						break;
-//						
-//					}
-//				}
-//				top = 1;
-//				System.out.println();
-//				
-//			}
+			
 			System.out.println("총 경우의 수 = " + list.size());
 			System.out.println("-----------------------------");
 			
--- childly/src/childly/Tree.java
+++ childly/src/childly/Tree.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 class Node {
 
+	// 각 노드들은 자신의 값과 상위 노드, 자식 노드를 필드로 가진다
 	private String data;
 	private Node parent;
 	private Node left;
@@ -46,17 +47,9 @@ class Node {
 
 public class Tree {
 
-	private Node root;
-	private int level = 0;
-	// 각 레벨에 해당하는 노드들을 저장
-	private ArrayList<Node> lastList = new ArrayList<>();
+	private int level = 0;		// 노드의 레벨
+	private ArrayList<Node> lastList = new ArrayList<>();		// 단말 노드들을 담는 리스트
 	
-	public Node getRoot() {
-		return root;
-	}
-	public void setRoot(Node root) {
-		this.root = root;
-	}
 	public int getLevel() {
 		return level;
 	}
@@ -66,25 +59,37 @@ public class Tree {
 	public ArrayList<Node> getLastList() {
 		return lastList;
 	}
-	public void addChild(Node node, int n, int level) {
 	
-		int a = countA(node);
-		int b = countB(node);
+	// A와 B의 개수를 계산해
+	// 재귀 호출 방식으로 자식 노드들을 생성한다
+	public void addChild(Node node, int n, int level) {
 		
+		/*
+		 * push 행동을 알파벳 A, pop 행동을 알파벳 B라고 생각했을때
+		 * A와 B의 개수는 같아야하며, A와 B를 나열할때 앞서는 A의 개수가 B의 개수보다 많거나 같아야한다
+		 * A의 개수는 n보다 작거나 같아야한다
+		 */
+
+		int a = countChar(node, 'A');
+		int b = countChar(node, 'B');
+		
+		// 어떤 노드가 A노드를 자식으로 가질 조건
 		if (n > a) {
 			Node childNode = new Node("A");
 			node.setLeft(childNode);
 			childNode.setParent(node);
-			this.addChild(childNode, n, level+1);
+			this.addChild(childNode, n, level+1);		// 생성한 자식노드를 다시 매개변수로 전달
 		}
 		
+		// 어떤 노드가 B노드를 자식으로 가질 조건
 		if (a > b) {
 			Node childNode = new Node("B");
 			node.setRight(childNode);
 			childNode.setParent(node);
 			this.addChild(childNode, n, level+1);
 		}
-		// 자식 노드들을 만들다가, 특히 최하위 레벨의 노드라면 리스트에 저장함
+		
+		// 특히, 단말 노드라면 리스트에 저장
 		// 마지막 레벨은 n*2-1
 		if (level == n*2-1) {
 			lastList.add(node);
@@ -92,6 +97,7 @@ public class Tree {
 		
 	}
 	
+	/*
 	public void clearTree(Node node) {
 		
 		if (node.getLeft() != null) {
@@ -109,7 +115,11 @@ public class Tree {
 		node = null;
 		
 	}
+	*/
 	
+	// 매개변수로 받은 어떤 노드에 대하여
+	// 그 노드의 상위 노드들을 추적하여
+	// 그 값들을 문자열로 만든다
 	public String chase(Node node) {
 		
 		if (node == null) {
@@ -121,40 +131,24 @@ public class Tree {
 		
 	}
 	
-	public int countA(Node node) {
+	// A와 B의 개수를 세는데 필요한 메소드
+	public int countChar(Node node, char ch) {
 		
-		if (node == null) {
-			return 0;
-		}
-		else {
+		String tmp = chase(node);
+		int cnt = 0;
+		
+		for (int i = 0; i < tmp.length(); i++) {
 			
-			if (node.getData() == "A") {
-				return 1 + countA(node.getParent());
-			}
-			else {
-				return countA(node.getParent());
-			}
+			if (tmp.charAt(i) == ch)
+				cnt++;
 			
 		}
+		
+		return cnt;
+		
 	}
 	
-	public int countB(Node node) {
-		
-		if (node == null) {
-			return 0;
-		}
-		else {
-			
-			if (node.getData() == "B") {
-				return 1 + countB(node.getParent());
-			}
-			else {
-				return countB(node.getParent());
-			}
-			
-		}
-	}
-	
+	// 노드 순회
 	public void inOrder(Node node) {
         if(node != null) {
             inOrder(node.getLeft());
